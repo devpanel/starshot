ab98f40896666d95053bf73465dc31566b60810f
kha <kha@devpanel.com>
2025-01-03 18:49:51 +0700

    Test

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -52,7 +52,8 @@ fi
 #== Pre-install starter recipe.
 if [ -d recipes/drupal_cms_starter ] && [ -z "$(mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME -e 'show tables')" ]; then
   while [ -z "$(drush status --fields=bootstrap)" ]; do
-    curl -Is "localhost/core/install.php?profile=drupal_cms_installer&langcode=en&recipes%5B0%5D=drupal_cms_starter&site_name=Drupal%20CMS" > /dev/null
+    echo "RUN CURL"
+    curl -Is "http://localhost/core/install.php?profile=drupal_cms_installer&langcode=en&recipes%5B0%5D=drupal_cms_starter&site_name=Drupal%20CMS" > /dev/null
   done
   drush ev "require_once 'core/includes/install.core.inc'; install_core_entity_type_definitions();"
   until drush recipe $APP_ROOT/recipes/drupal_cms_starter; do
