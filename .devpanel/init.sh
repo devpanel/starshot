5ee0c5e1d36adea0843bf268f2cce4a6aa6284db
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2025-01-15 17:07:39 -0500

    Add logging to install script

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -15,68 +15,161 @@
 # For GNU Affero General Public License see <https://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
 
+LOG_FILE="$APP_ROOT/logs/init-$(date +%F-%T).log"
+exec > >(tee $LOG_FILE) 2>&1
+
+TIMEFORMAT=%lR
 DDEV_DOCROOT=${WEB_ROOT##*/}
 SETTINGS_FILE_PATH=$WEB_ROOT/sites/default/settings.php
 
 #== Clone source code.
 if [ -z "$(ls -A $APP_ROOT/repos/drupal/drupal_cms)" ]; then
-  git submodule update --init --remote --recursive
+  echo
+  time git submodule update --init --remote --recursive
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
+
   cd $APP_ROOT/repos/drupal/drupal_cms
-  git checkout $(git branch -r | grep "origin/HEAD" | cut -f 3 -d '/')
+  echo
+  time git checkout $(git branch -r | grep "origin/HEAD" | cut -f 3 -d '/')
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 
   #== Patch for issue #3497485.
   if ! git merge-base --is-ancestor 86d48c24bdf96494a8a017d15c368574794d580a HEAD 2> /dev/null; then
-    git apply $APP_ROOT/patches/drupal/drupal_cms/373.patch
+    echo
+    echo 'Apply patch for issue #3497485.'
+    time git apply $APP_ROOT/patches/drupal/drupal_cms/373.patch
+    RETURN_CODE=$?
+    if [ $RETURN_CODE != 0 ]; then
+      exit $RETURN_CODE
+    fi
   fi
 fi
 
 #== Remove root-owned files.
 cd $APP_ROOT
-sudo rm -rf lost+found
+echo
+echo Remove root-owned files.
+time sudo rm -rf lost+found
+RETURN_CODE=$?
+if [ $RETURN_CODE != 0 ]; then
+  exit $RETURN_CODE
+fi
 
 #== Composer install.
 if [ ! -f composer.lock ]; then
-  .devpanel/generate-composer-json > composer.json
-  composer install
+  echo
+  echo 'Generate composer.json.'
+  time .devpanel/generate-composer-json > composer.json
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
+
+  echo
+  time composer -n install --no-progress
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
 
-#== Symlink the installer into the web root.
-ln -s -f $(realpath -s --relative-to=$DDEV_DOCROOT/profiles repos/drupal/drupal_cms/project_template/$DDEV_DOCROOT/profiles/drupal_cms_installer) $DDEV_DOCROOT/profiles
+#== Symlink Drupal CMS installer into web root.
+echo
+echo 'Symlink Drupal CMS installer into web root.'
+time ln -s -f $(realpath -s --relative-to=$DDEV_DOCROOT/profiles repos/drupal/drupal_cms/project_template/$DDEV_DOCROOT/profiles/drupal_cms_installer) $DDEV_DOCROOT/profiles
+RETURN_CODE=$?
+if [ $RETURN_CODE != 0 ]; then
+  exit $RETURN_CODE
+fi
 
 #== Install JavaScript dependencies if needed.
 cd $APP_ROOT/repos/drupal/drupal_cms
 if [ ! -d node_modules ]; then
-  npm clean-install --foreground-scripts
+  time npm -q clean-install
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
 
 #== Build Experience Builder's JavaScript bundle, if needed.
+cd $APP_ROOT
 XB_UI_PATH=$DDEV_DOCROOT/modules/contrib/experience_builder/ui
-if [ -d $XB_UI_PATH/dist ]; then
-  npm --prefix $XB_UI_PATH install
-  npm --prefix $XB_UI_PATH run build
+if [ ! -d $XB_UI_PATH/dist ]; then
+  echo
+  time npm -q --prefix $XB_UI_PATH install
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
+  echo
+  time npm -q --prefix $XB_UI_PATH run build
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
 
 #== Create the private files directory.
-cd $APP_ROOT
 if [ ! -d private ]; then
-  mkdir private
+  echo
+  echo 'Create the private files directory.'
+  time mkdir private
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
 
 #== Set up settings.php file.
 if [ ! -f $SETTINGS_FILE_PATH ]; then
-  echo "Set up settings.php file."
-  cp $APP_ROOT/.devpanel/drupal-settings.php $SETTINGS_FILE_PATH
+  echo
+  echo 'Set up settings.php file.'
+  time cp $APP_ROOT/.devpanel/drupal-settings.php $SETTINGS_FILE_PATH
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
 
 #== Pre-install starter recipe.
-cd $APP_ROOT
 if [ -d recipes/drupal_cms_starter ] && [ -z "$(mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME -e 'show tables')" ]; then
-  while [ -z "$(drush status --fields=bootstrap)" ]; do
-    .devpanel/install > /dev/null
+  echo
+  echo 'Install Drupal base system.'
+  while [ -z "$(drush sget drupal_cms_profile.profile_modules_installed 2> /dev/null)" ]; do
+    time .devpanel/install > /dev/null
   done
-  until drush recipe $APP_ROOT/recipes/drupal_cms_starter; do
+  drush sdel drupal_cms_profile.profile_modules_installed
+
+  echo
+  echo 'Apply the Drupal CMS starter recipe.'
+  until time drush -q recipe $APP_ROOT/recipes/drupal_cms_starter; do
     :
   done
-  drush -n pmu drupal_cms_installer
-  drush cr
+
+  echo
+  time drush -n pmu drupal_cms_installer
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
+
+  echo
+  time drush cr
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    exit $RETURN_CODE
+  fi
 fi
+
+INIT_DURATION=$SECONDS
+INIT_HOURS=$(($INIT_DURATION / 3600))
+INIT_MINUTES=$(($INIT_DURATION % 3600 / 60))
+INIT_SECONDS=$(($INIT_DURATION % 60))
+printf "\nTotal elapsed time: %d:%02d:%02d\n" $INIT_HOURS $INIT_MINUTES $INIT_SECONDS
